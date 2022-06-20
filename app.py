f3307edf1f927f34dcc97bc7898f6551078114ec
Camil <camil@coinafrique.com>
2022-06-20 10:11:41 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, static_folder='static')
+
+# WEBSITE_HOSTNAME exists only in production environment
+if not 'WEBSITE_HOSTNAME' in os.environ:
+   # local development, where we'll use environment variables
+   print("Loading config.development and environment variables from .env file.")
+   app.config.from_object('azureproject.development')
+else:
+   # production
+   print("Loading config.production.")
+   app.config.from_object('azureproject.production')
+
+
+ @app.route('/', methods=['GET'])
+def index():
+    print('Request for index page received')
+    return render_template('index.html')
+
+@app.route('/resultat/<str:id>', methods=['GET'])
+def details(id):
+	if id == "LRr7NFfJ4HTKGOhvfqCJURsW66s1txd339tIc1ueyhXYbNdcgOHd3bsTpu0HdefvuQHwkyjeUobDSsKiSgKEPYYRGzjG0L0k2suk":
+    	return render_template('result.html')
+    else:
+    	return render_template('not_found.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+   app.run()
